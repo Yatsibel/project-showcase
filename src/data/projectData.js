4ad64912a519294e5cc26a81eaba55481e855e6e
yop_08 <yatsibeloyolaperez@gmail.com>
2025-08-25 19:04:31 -0400

    Update projectData.js

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -2,17 +2,16 @@
 export const projects = [
   {
     id: 1,
-    title: {Cambio Climatico
-      en: "",
-      es: "",
+    title: {
+      en: "Climate Change",
+      es: "Cambio Climático",
     },
     description: {
-      en: "",
-      es: "",
+      en: "A website explaining what climate change is, its causes, consequences, and solutions.",
+      es: "Un sitio web que explica qué es el cambio climático, sus causas, consecuencias y soluciones.",
     },
-    image: "",
-    liveSite: "",
-    githubRepo: "",
+    image: "",   
+    liveSite: \
+    githubRepo:  https://yatsibel.github.io/Cambio-Climatico/
   },
-  // Add more projects here...
 ];
